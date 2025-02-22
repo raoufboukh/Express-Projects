1c31cb92ba7749025f7fd07cb2b3267eb9b1280c
raoufboukh <zakib2316@gmail.com>
2025-02-22 01:20:34 +0100

    tokens

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import router from "./routers/auth.routers.js";
+import userRouter from "./routers/user.routers.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use('/auth',router);
+app.use('/users', userRouter);
+
+const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL).then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+        console.log("Server is running on port 5000");
+    })
+})
+
