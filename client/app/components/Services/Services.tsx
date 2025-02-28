f56f0ef01e2f02c9fdaa9009ca52e10ae4b77c79
raoufboukh <zakib2316@gmail.com>
2025-02-28 18:15:57 +0100

    services component

---
+++ client/app/components/Services/Services.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { services } from '../constants';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Services = () => {
+  return (
+    <section className='bg-white'>
+      <div className="container px-4 mx-auto lg:px-28">
+        <h2 
+            className='text-6xl font-bold my-20'
+        >
+            Services Takes good <br/> care of you
+        </h2>
+        <div className='grid grid-cols-1 lg:grid-cols-4 md:grid-cols-2 gap-5 mb-10'>
+            {services.map((service, i) => (
+                <div key={i}>
+                    <Image src={service.image} alt={service.title} width={100} height={100} />
+                    <h3 className='text-2xl my-5'>{service.title}</h3>
+                    <p className='text-gray-400'>{service.description}</p>
+                </div>
+            ))}
+        </div>
+        <Link href='/services' className='block w-fit mx-auto px-20 my-20 py-2 bg-black/80 text-white'>View All</Link>
+      </div>
+    </section>
+  );
+}
+
+export default Services
--- client/app/components/constants.ts
+++ client/app/components/constants.ts
@@ -24,3 +24,30 @@ export const links = [
         link: "/login"
     },
 ]
+
+export const services = [
+  {
+    image: "/assets/home_clinic3_pic1.png",
+    title: "Stomatology",
+    description:
+      "Duis dignissim mi ut laoreet mollis. Nunc id tellus finibus, eleifend mi maximus",
+  },
+  {
+    image: "/assets/home_clinic3_pic2.png",
+    title: "Vaccination",
+    description:
+      "Duis dignissim mi ut laoreet mollis. Nunc id tellus finibus, eleifend mi maximus",
+  },
+  {
+    image: "/assets/home_clinic3_pic3.png",
+    title: "Check-up",
+    description:
+      "Duis dignissim mi ut laoreet mollis. Nunc id tellus finibus, eleifend mi maximus",
+  },
+  {
+    image: "/assets/home_clinic3_pic4.png",
+    title: "Medical Help",
+    description:
+      "Duis dignissim mi ut laoreet mollis. Nunc id tellus finibus, eleifend mi maximus",
+  },
+];
