1dc07d328c6c4d2f5ee4cafd07460464b298dc0a
raoufboukh <zakib2316@gmail.com>
2025-03-10 15:44:10 +0100

    give access the client from the server

--- server/index.js
+++ server/index.js
@@ -9,7 +9,13 @@ import userRouter from "./routers/user.routers.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use('/auth',router);
