52e1ea89fbd98070920fccf1abb9e6a243f25066
raoufboukh <zakib2316@gmail.com>
2025-03-15 23:48:10 +0100

    fix appointments and add bookAppointment

--- client/components/Appointment/Appointments.tsx
+++ client/components/Appointment/Appointments.tsx
@@ -1,31 +1,78 @@
+"use client";
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { check } from "@/lib/dataFetching";
-import { useQuery } from "@tanstack/react-query";
+import { cancelAppointment, check } from "@/lib/dataFetching";
+import { useMutation } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 
 const Appointments = () => {
-  const { data, isLoading } = useQuery({
-    queryKey: ["appointments"],
-    queryFn: check,
+  const [id, setId] = useState("");
+  const [user, setUser] = useState<any>({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchUserData = async () => {
+    try {
+      setIsLoading(true);
+      const data = await check();
+      setUser(data);
+    } catch (err) {
+      console.error("Error during auth check:", err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  const { mutate, isPending } = useMutation({
+    mutationKey: ["appointments"],
+    mutationFn: () => cancelAppointment(id),
+    onSuccess: () => {
+      fetchUserData();
+      setId("");
+    },
   });
+
   return (
     <div>
       {isLoading ? (
         <div className="text-white text-3xl">Chargement...</div>
-      ) : data.appointments.length !== 0 ? (
+      ) : user.appointments && user?.appointments?.length !== 0 ? (
         <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-2 overflow-y-clip">
-          {data.map((item: any, i: number) => (
+          {user.appointments.map((item: any, i: number) => (
             <div
               key={i}
               className="text-white bg-gray-800 p-4 rounded-md shadow-md flex flex-col justify-between px-10 sm:text-base text-sm"
             >
               <p>
                 <span className="text-gray-300">date: </span>
-                {item.date}
+                {item.date.slice(0, 10)}
               </p>
-              <p className={item.status === "pending" ? "text-orange-500" : item.status === "accepted" ? "text-green-500" : "text-red-500"}>
+              <p
+                className={
+                  item.status === "pending"
+                    ? "text-orange-500"
+                    : item.status === "accepted"
+                    ? "text-green-500"
+                    : "text-red-500"
+                }
+              >
                 <span className="text-gray-300">Status: </span>
                 {item.status}
               </p>
+              <button
+                className="cursor-pointer block bg-primary px-2 py-1 mt-5 rounded-sm transition-all duration-300 hover:bg-primary/80 disabled:opacity-50"
+                onClick={() => {
+                  setId(item._id);
+                  mutate();
+                }}
+                disabled={item.status === "accepted" || isPending}
+              >
+                {isPending && id === item._id
+                  ? "Annulation..."
+                  : "Cancel Appointment"}
+              </button>
             </div>
           ))}
         </div>
---
+++ client/components/Appointment/BookApp.tsx
@@ -0,0 +1,68 @@
+"use client";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { bookAppointment, check } from "@/lib/dataFetching";
+import { useMutation } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css"; // Ajout des styles CSS pour le DatePicker
+
+const BookApp = () => {
+    const [startDate, setStartDate] = useState<Date | null>(null);
+    const [user, setUser] = useState<any>({});
+    useEffect(() => {
+        const checkAuth = async () => {
+            try {
+                const data = await check();
+                setUser(data);
+            } catch (err) {
+                console.error("Error during auth check:", err);
+            }
+        };
+        checkAuth();
+    },[user]);
+    const { mutate, isPending } = useMutation({
+        mutationKey: ["bookAppointment"],
+        mutationFn: () => {
+            if (startDate) {
+                return bookAppointment(startDate);
+            }
+            throw new Error("Start date is null");
+        },
+    })
+  return (
+    <div className="text-white">
+      <DatePicker
+        selected={startDate}
+        onChange={(date: Date | null) => {
+          if (date) {
+            const newDate = new Date(
+              date.getFullYear(),
+              date.getMonth(),
+              date.getDate(),
+              12,
+              0,
+              0
+            );
+            setStartDate(newDate);
+          }
+        }}
+        inline
+      />
+      <button
+        className="cursor-pointer block bg-primary px-2 py-1 rounded-sm transition-all duration-300 hover:bg-primary/80"
+        onClick={() => {
+          if (startDate) {
+            mutate();
+          }
+        }}
+        disabled={isPending}
+      >
+        Book
+      </button>
+      {isPending && <p>Loading...</p>}
+      {user.appointments && user?.appointments[0]?.date && <p>You are booking at {user.appointments[0].date.slice(0,10)}</p>}
+    </div>
+  );
+}
+
+export default BookApp
