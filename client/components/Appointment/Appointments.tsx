fdbc4d00bdb6da025c99220c1e72457cfc39b74d
raoufboukh <zakib2316@gmail.com>
2025-03-22 23:07:42 +0100

    restructure appointment booking components and add form handling

--- client/components/Appointment/Appointments.tsx
+++ client/components/Appointment/Appointments.tsx
@@ -40,23 +40,42 @@ function Appointments() {
       {isLoading ? (
         <div className="text-white text-3xl">Chargement...</div>
       ) : user.appointments && user?.appointments?.length !== 0 ? (
-        <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-2 overflow-y-clip">
+        <div
+          className={`${
+            user.appointments.length > 2 && "h-96"
+          } grid grid-cols-1 lg:grid-cols-2 gap-2 overflow-y-auto`}
+        >
           {user.appointments.map((item: any, i: number) => (
             <div
               key={i}
               className="text-white bg-gray-800 p-4 rounded-md shadow-md flex flex-col justify-between px-10 sm:text-base text-sm"
             >
               <p>
-                <span className="text-gray-300">date: </span>
+                <span className="text-gray-300">FirstName: </span>
+                {item.firstName}
+              </p>
+              <p>
+                <span className="text-gray-300">LastName: </span>
+                {item.lastName}
+              </p>
+              <p>
+                <span className="text-gray-300">Number: </span>0{item.number}
+              </p>
+              <p>
+                <span className="text-gray-300">Date: </span>
                 {item.date.slice(0, 10)}
+              </p>
+              <p>
+                <span className="text-gray-300">Time: </span>
+                {item.time}
               </p>
               <p
                 className={
                   item.status === "pending"
                     ? "text-orange-500"
                     : item.status === "accepted"
-                      ? "text-green-500"
-                      : "text-red-500"
+                    ? "text-green-500"
+                    : "text-red-500"
                 }
               >
                 <span className="text-gray-300">Status: </span>
