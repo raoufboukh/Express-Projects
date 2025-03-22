091b1200bd356f66376a1a765192e32d209e1fd4
raoufboukh <zakib2316@gmail.com>
2025-03-22 23:00:31 +0100

    create button can addUser

--- client/components/Doctors/Doctors.tsx
+++ client/components/Doctors/Doctors.tsx
@@ -1,43 +1,50 @@
 import { useQuery } from "@tanstack/react-query";
-
 import { getDoctors } from "@/lib/data-fetching";
+import { useState } from "react";
+import AddUser from "../Users/AddUser";
 
 function Doctors() {
+  const [show, setShow] = useState(false);
   const { data, isLoading } = useQuery({
     queryKey: ["doctors"],
     queryFn: getDoctors,
   });
   return (
     <div>
-      {isLoading
-        ? (
-            <div className="text-white text-3xl">Chargement...</div>
-          )
-        : data.length !== 0
-          ? (
-              <div className="grid grid-cols-1 gap-2 overflow-y-clip">
-                {data.map((item: any, i: number) => (
-                  <div
-                    key={i}
-                    className="text-white bg-gray-800 p-4 rounded-md shadow-md flex flex-col justify-between px-10"
-                  >
-                    <p>
-                      <span className="text-gray-300">Username: </span>
-                      {item.username.charAt(0).toUpperCase() + item.username.slice(1)}
-                    </p>
-                    <p>
-                      <span className="text-gray-300">Email: </span>
-                      {item.email}
-                    </p>
-                  </div>
-                ))}
-              </div>
-            )
-          : (
-              <div className="text-white bg-gray-800 p-4 rounded-md">
-                Aucun doctor trouvé
-              </div>
-            )}
+      {isLoading ? (
+        <div className="text-white text-3xl">Chargement...</div>
+      ) : data.length !== 0 ? (
+        <div className="flex flex-col gap-2 overflow-y-auto h-96">
+          {data.map((item: any, i: number) => (
+            <div
+              key={i}
+              className="text-white bg-gray-800 p-4 rounded-md shadow-md flex flex-col justify-between px-10"
+            >
+              <p>
+                <span className="text-gray-300">Username: </span>
+                {item.username.charAt(0).toUpperCase() + item.username.slice(1)}
+              </p>
+              <p>
+                <span className="text-gray-300">Email: </span>
+                {item.email}
+              </p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="text-white bg-gray-800 p-4 rounded-md">
+          Aucun doctor trouvé
+        </div>
+      )}
+      {!isLoading && (
+        <button
+          className="bg-primary hover:bg-primary/80 block w-fit mx-auto cursor-pointer text-white px-4 py-2 rounded-md mt-4"
+          onClick={() => setShow(!show)}
+        >
+          Add
+        </button>
+      )}
+      {show && <AddUser setShow={setShow} />}
     </div>
   );
 }
--- client/components/Users/Users.tsx
+++ client/components/Users/Users.tsx
@@ -1,43 +1,50 @@
 import { useQuery } from "@tanstack/react-query";
-
 import { getUsers } from "@/lib/data-fetching";
+import { useState } from "react";
+import AddUser from "./AddUser";
 
 function Users() {
+  const [show, setShow] = useState(false);
   const { data, isLoading } = useQuery({
     queryKey: ["users"],
     queryFn: getUsers,
   });
   return (
-    <div>
-      {isLoading
-        ? (
-            <div className="text-white text-3xl">Chargement...</div>
-          )
-        : data.length !== 0
-          ? (
-              <div className="grid grid-cols-1 gap-2 overflow-y-clip">
-                {data.map((item: any) => (
-                  <div
-                    key={item.id}
-                    className="text-white bg-gray-800 p-4 rounded-md shadow-md flex flex-col justify-between px-10 sm:text-base text-sm"
-                  >
-                    <p>
-                      <span className="text-gray-300">Username: </span>
-                      {item.username.charAt(0).toUpperCase() + item.username.slice(1)}
-                    </p>
-                    <p>
-                      <span className="text-gray-300">Email: </span>
-                      {item.email}
-                    </p>
-                  </div>
-                ))}
-              </div>
-            )
-          : (
-              <div className="text-white bg-gray-800 p-4 rounded-md">
-                Aucun utilisateur trouvé
-              </div>
-            )}
+    <div className="relative">
+      {isLoading ? (
+        <div className="text-white text-3xl">Chargement...</div>
+      ) : data.length !== 0 ? (
+        <div className="flex flex-col gap-2 overflow-y-auto h-96">
+          {data.map((item: any, i: number) => (
+            <div
+              key={i}
+              className="text-white bg-gray-800 p-4 rounded-md shadow-md flex flex-col justify-between px-10 sm:text-base text-sm"
+            >
+              <p>
+                <span className="text-gray-300">Username: </span>
+                {item.username.charAt(0).toUpperCase() + item.username.slice(1)}
+              </p>
+              <p>
+                <span className="text-gray-300">Email: </span>
+                {item.email}
+              </p>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="text-white bg-gray-800 p-4 rounded-md">
+          Aucun utilisateur trouvé
+        </div>
+      )}
+      {!isLoading && (
+        <button
+          className="bg-primary hover:bg-primary/80 block w-fit mx-auto cursor-pointer text-white px-4 py-2 rounded-md mt-4"
+          onClick={() => setShow(!show)}
+        >
+          Add
+        </button>
+      )}
+      {show && <AddUser setShow={setShow} />}
     </div>
   );
 }
