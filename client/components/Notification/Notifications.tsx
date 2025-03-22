1ba153e97382ab4e6467f0800e9d3018c348ec8b
raoufboukh <zakib2316@gmail.com>
2025-03-22 23:11:34 +0100

    add first ,last names and number at notifications

--- client/components/Notification/Notifications.tsx
+++ client/components/Notification/Notifications.tsx
@@ -47,62 +47,78 @@ function Notifications() {
   });
   return (
     <div>
-      {isLoading
-        ? (
-            <div className="text-white text-3xl">Chargement...</div>
-          )
-        : data && data.length !== 0
-          ? (
-              <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-2 overflow-y-clip">
-                {data
-                  && data.map((item: any) => (
-                    <div
-                      key={item._id}
-                      className="text-white bg-gray-800 p-4 rounded-md shadow-md flex flex-col justify-between px-10 sm:text-base text-sm"
-                    >
-                      <p>
-                        <span className="text-gray-300">Message: </span>
-                        {item.message}
-                      </p>
-                      <p>
-                        <span className="text-gray-300">date: </span>
-                        {item.appointment.date.slice(0, 10)}
-                      </p>
-                      <div className="flex justify-between mt-5">
-                        <button
-                          type="button"
-                          className="bg-primary text-white px-2 py-1 rounded-md cursor-pointer transition-all duration-300 hover:bg-primary/80 disabled:opacity-50"
-                          onClick={() => {
-                            setId(item._id);
-                            setMessage("Accepter");
-                            mutate();
-                          }}
-                          disabled={isPending}
-                        >
-                          Accepter
-                        </button>
-                        <button
-                          type="button"
-                          className="bg-red-500 text-white px-2 py-1 rounded-md cursor-pointer transition-all duration-300 hover:bg-red-500/80"
-                          onClick={() => {
-                            setId(item._id);
-                            setMessage("Refuser");
-                            mutate();
-                          }}
-                          disabled={isPending}
-                        >
-                          Refuser
-                        </button>
-                      </div>
-                    </div>
-                  ))}
+      {isLoading ? (
+        <div className="text-white text-3xl">Chargement...</div>
+      ) : data && data.length !== 0 ? (
+        <div
+          className={`${
+            data.length > 2 && "h-96"
+          } grid grid-cols-1 lg:grid-cols-2 gap-2 overflow-y-auto`}
+        >
+          {data &&
+            data.map((item: any) => (
+              <div
+                key={item._id}
+                className="text-white bg-gray-800 p-4 rounded-md shadow-md flex flex-col justify-between px-10 sm:text-base text-sm"
+              >
+                <p>
+                  <span className="text-gray-300">Message: </span>
+                  {item.message}
+                </p>
+                <p>
+                  <span className="text-gray-300">FirstName: </span>
+                  {item.appointment.firstName}
+                </p>
+                <p>
+                  <span className="text-gray-300">LastName: </span>
+                  {item.appointment.lastName}
+                </p>
+                <p>
+                  <span className="text-gray-300">Number: </span>
+                  {item.appointment.number}
+                </p>
+                <p>
+                  <span className="text-gray-300">Time: </span>
+                  {item.appointment.time}
+                </p>
+                <p>
+                  <span className="text-gray-300">date: </span>
+                  {item.appointment.date.slice(0, 10)}
+                </p>
+                <div className="flex justify-between mt-5">
+                  <button
+                    type="button"
+                    className="bg-primary text-white px-2 py-1 rounded-md cursor-pointer transition-all duration-300 hover:bg-primary/80 disabled:opacity-50"
+                    onClick={() => {
+                      setId(item._id);
+                      setMessage("Accepter");
+                      mutate();
+                    }}
+                    disabled={isPending}
+                  >
+                    Accepter
+                  </button>
+                  <button
+                    type="button"
+                    className="bg-red-500 text-white px-2 py-1 rounded-md cursor-pointer transition-all duration-300 hover:bg-red-500/80"
+                    onClick={() => {
+                      setId(item._id);
+                      setMessage("Refuser");
+                      mutate();
+                    }}
+                    disabled={isPending}
+                  >
+                    Refuser
+                  </button>
+                </div>
               </div>
-            )
-          : (
-              <div className="text-white bg-gray-800 p-4 rounded-md">
-                Aucune notification
-              </div>
-            )}
+            ))}
+        </div>
+      ) : (
+        <div className="text-white bg-gray-800 p-4 rounded-md">
+          Aucune notification
+        </div>
+      )}
     </div>
   );
 }
