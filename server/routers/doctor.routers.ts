6c0a320d4dca50c432585a65deba7cba773cc6f0
7afde <hafed.zouyed@univ-constantine2.dz>
2025-05-07 15:31:42 +0100

    fix: Correct doctor route to use addDoctor for POST requests

--- server/routers/doctor.routers.ts
+++ server/routers/doctor.routers.ts
@@ -1,9 +1,9 @@
-import { getDoctors } from "controllers/user.controllers.ts";
+import { addDoctor, getDoctors } from "controllers/user.controllers.ts";
 import express from "express";
 
 const doctorRouter = express.Router();
 
 doctorRouter.get("/", getDoctors);
-doctorRouter.post("/", getDoctors);
+doctorRouter.post("/", addDoctor);
 
 export default doctorRouter;
